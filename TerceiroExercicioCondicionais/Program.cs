ee522cde361e8319e51042c814793789593baeb2
gabsoares <soaresgabriel68@gmail.com>
2024-04-18 16:37:19 -0300

    Add project files.

---
+++ TerceiroExercicioCondicionais/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade > 0)
+{
+    if (idade >= 18)
+    {
+        Console.WriteLine("Maior de idade");
+    }
+    else
+    {
+        Console.WriteLine("Menor de idade");
+    }
+}
+else
+{
+    Console.WriteLine("Idade invalida");
+}
